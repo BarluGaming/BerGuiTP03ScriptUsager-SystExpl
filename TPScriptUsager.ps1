85bf23f4b3d941e68655988e641a339b3e209e53
Guillaume Berube <2194955@csfoy.ca>
2021-12-16 00:07:25 -0500

    Edit 1

---
+++ TPScriptUsager.ps1
@@ -0,0 +1,16 @@
+# Travail Pratique 3 - Creating users through scripting
+# Travail remis dans le cadre du cours Systèmes d'exploitation 
+# 420-W12-SF
+# 17 Décembre 2021
+
+# Début du script
+Clear-Host
+Set-Location C:\TPSE\
+
+# Prévention des erreurs
+$ErrorActionPreference = "Stop"
+
+# Déclaration des variables
+$Names = Get-Content .\nom.csv
+$Group = "Entreprise"
+Write-Host $Entreprise
